71fe7ecd52a45d325b34dfe018dd57dc6a5e6cdf
Ashwin245-png <mmkarrie554@gmail.com>
2024-09-03 20:38:10 +0530

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -28,7 +28,7 @@ urlpatterns = [
     path("receipe/", receipe, name='receipe'),
     path("delete-receipe/<id>/", delete_receipe, name='delete_receipe'),
     path('update-receipe/<id>/', update_receipe, name='update_receipe'),
-    path('login-', login_page, name='login_page'),
+    path('login/', login_page, name='login_page'),
     path('register/', register, name='register'), 
     path('logout/', logout_page, name='logout_page')
 ]
